9baac803b3b09dc60bc94bf45f51ea9e86c5e6aa
Bruno dos Reis Souza <bruno.souza@spread.com.br>
2019-07-18 16:49:42 -0300

    quase

--- src/main/java/br/com/reciverb2w/reciverb2w/App.java
+++ src/main/java/br/com/reciverb2w/reciverb2w/App.java
@@ -1,40 +1,22 @@
 package br.com.reciverb2w.reciverb2w;
 
-import java.util.Date;
-
-import br.com.reciverb2w.reciverb2w.converteminuto.ConvertMinut;
-
 public class App 
 {
     public static void main( String[] args ){
-    	
-    	
-    	
     	//long tempoInicio = System.currentTimeMillis();
-    	
-    	ConvertMinut covert = new ConvertMinut();
-    	
-    	int minutoRetornoInicio = covert.minutConv();
-    	
     	ConsultaUrl consulta = new ConsultaUrl(); 
     	
     	//600000 vezes
 		/* for(int minutoRetornoInicio; ) */
     	
-    	for(int i = 0; i < 30; i++) {
+    	
+    		consulta.consultaServer();
     		
-    		try {
-    			consulta.consultaServer();
-    			Thread.sleep(2000);
-    		}catch (InterruptedException e)
 			
-    		{	//Erro na execução.
-				System.out.println("ERRO");
-			}
-			
-			//Mostra a contagem.
-			System.out.println("Contando:" + i);
-		}
+		
+		 
+		 
+	
     	
     	//Date minutoRetornoFinal = covert.minutConv();
     	
--- src/main/java/br/com/reciverb2w/reciverb2w/ConsultaUrl.java
+++ src/main/java/br/com/reciverb2w/reciverb2w/ConsultaUrl.java
@@ -1,12 +1,8 @@
 package br.com.reciverb2w.reciverb2w;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -14,9 +10,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Scanner;
 
 import com.opencsv.CSVWriter;
 import com.opencsv.bean.StatefulBeanToCsv;
@@ -31,16 +25,14 @@ public class ConsultaUrl {
 	
 	private static final String CSV_FILE_PATH = "dadosserv/dados_url.csv"; 
 	 
-	@SuppressWarnings("resource")
 	public void consultaServer() {
-		for(int i = 0; i < 30; i++) {
+	/*	for(int i = 0; i < 3; i++) {*/
 	    	
 			try {
 				
-				
 	    		Integer count200 = 0;
-	    	 	Integer count400 = 0;
-	    	 	Integer count500 = 0;
+	    		Integer count400 = 0;
+	    		Integer count500 = 0;
 	    		
 
 	    		
@@ -57,64 +49,47 @@ public class ConsultaUrl {
 	    		switch (response.getStatus()) {
 				
 	    		case 200:
-					//JSONObject objectJson = new JSONObject();
 					try {
-						
-						File file = new File(CSV_FILE_PATH);
-						//Scanner sc = new Scanner(System.in);
-						
-						FileWriter outputfile = new FileWriter(file); 
-						  CSVWriter writer = new CSVWriter(outputfile, ';', 
-                                  CSVWriter.NO_QUOTE_CHARACTER, 
-                                  CSVWriter.DEFAULT_ESCAPE_CHARACTER, 
-                                  CSVWriter.DEFAULT_LINE_END); 
-						  
-							List<QtdSolicitacao> solicitacao = new ArrayList<QtdSolicitacao>();
-							List<String[]> data = new ArrayList<String[]>(); 
-							Date date = new Date();
-							
-							String dataInfo = dateFormat.format(date);
-							
-							data.add(new String[] {dataInfo});
-							
-							solicitacao.add(new QtdSolicitacao(dataInfo, count200, count400, count500));
-							
-							System.out.println("Enter no of rows"); 
-							
-							//int noOfRow = Integer.parseInt(); 
-							System.out.println("Enter Data");
-							
-			
-						
 
-						writer.flush();
-						writer.close();
-						//System.out.println("oioio");
+							Writer writer = Files.newBufferedWriter(Paths.get(CSV_FILE_PATH));
+							List<QtdSolicitacao> solicitacao = new ArrayList<QtdSolicitacao>();
+							
+							 for(int z = 0; z < 3; z++) {
+								 	count200++;
+								 	Date date = new Date();
+									String dataInfo = dateFormat.format(date);
+									solicitacao.add(new QtdSolicitacao(dataInfo, count200, count400, count500));
+									Thread.sleep(2000);
+									System.out.println("Contando:" + z);
+							 }
+							 StatefulBeanToCsv<QtdSolicitacao> beanToCsv = new StatefulBeanToCsvBuilder(writer).build();
+							 beanToCsv.write(solicitacao);
+							
+							writer.flush();
+							writer.close();
+						
 					} catch (IOException e) { 
 			            // TODO Auto-generated catch block 
 			            e.printStackTrace(); 
 			        } 
 					break;
-				case 201:
+				case 400:
 					
-
+				break;
+				
+				
+				case 500:
+					
 				default:
 					break;
 				}
-	    		/*
-				 * if(response.getStatus() == 200) { System.out.println("ok"+ " 200"); }else {
-				 * throw new RuntimeException("Failed : HTTP erro code: " +
-				 * response.getStatus()); }
-				 */
+	   
 	    		
-	    		//String output = response.getEntity(String.class);
-	    		//System.out.println("Output from Server ....\n");
-	    		//System.out.println(output);
-	    		Thread.sleep(2000);
 	    	}catch (Exception e) {
 				// TODO: handle exception
 	    		e.printStackTrace();
 			}
-		}
+			
+		
 	}
 }
