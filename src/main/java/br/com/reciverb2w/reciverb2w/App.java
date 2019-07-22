991b9d6d9ed82b13efbeff5d58f08d7b8e290c3f
Bruno dos Reis Souza <bruno.souza@spread.com.br>
2019-07-22 16:41:50 -0300

    falta pouco

--- src/main/java/br/com/reciverb2w/reciverb2w/App.java
+++ src/main/java/br/com/reciverb2w/reciverb2w/App.java
@@ -3,25 +3,9 @@ package br.com.reciverb2w.reciverb2w;
 public class App 
 {
     public static void main( String[] args ){
-    	//long tempoInicio = System.currentTimeMillis();
+
     	ConsultaUrl consulta = new ConsultaUrl(); 
-    	
-    	//600000 vezes
-		/* for(int minutoRetornoInicio; ) */
-    	
-    	
-    		consulta.consultaServer();
-    		
-			
-		
-		 
-		 
-	
-    	
-    	//Date minutoRetornoFinal = covert.minutConv();
-    	
-    	
-    	    
+    	consulta.consultaServer(); 
     }
     
    
--- src/main/java/br/com/reciverb2w/reciverb2w/ConsultaUrl.java
+++ src/main/java/br/com/reciverb2w/reciverb2w/ConsultaUrl.java
@@ -1,7 +1,5 @@
 package br.com.reciverb2w.reciverb2w;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
 import java.nio.file.Files;
@@ -9,10 +7,11 @@ import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Currency;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Logger;
 
-import com.opencsv.CSVWriter;
 import com.opencsv.bean.StatefulBeanToCsv;
 import com.opencsv.bean.StatefulBeanToCsvBuilder;
 import com.sun.jersey.api.client.Client;
@@ -24,18 +23,16 @@ import br.com.reciverb2w.model.QtdSolicitacao;
 public class ConsultaUrl {
 	
 	private static final String CSV_FILE_PATH = "dadosserv/dados_url.csv"; 
-	 
+
+	static Logger logger = Logger.getLogger(Currency.class.getName());
+	
 	public void consultaServer() {
-	/*	for(int i = 0; i < 3; i++) {*/
+		Integer count200 = 0;
+		Integer count400 = 0;
+		Integer count500 = 0;
 	    	
 			try {
-				
-	    		Integer count200 = 0;
-	    		Integer count400 = 0;
-	    		Integer count500 = 0;
-	    		
-
-	    		
+			
 	    		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	    		
 	    		Client client = Client.create();
@@ -43,50 +40,57 @@ public class ConsultaUrl {
 	    		WebResource webUrl = client.resource("https://desafioperformance.b2w.io/bairros");
 	    		
 	    		ClientResponse response = webUrl.accept("application/json")
-	                    .get(ClientResponse.class);   
+	                    .get(ClientResponse.class); 
 	    		
+	    		Writer writer = Files.newBufferedWriter(Paths.get(CSV_FILE_PATH));
+				List<QtdSolicitacao> solicitacao = new ArrayList<QtdSolicitacao>();
 				
-	    		switch (response.getStatus()) {
-				
-	    		case 200:
-					try {
-
-							Writer writer = Files.newBufferedWriter(Paths.get(CSV_FILE_PATH));
-							List<QtdSolicitacao> solicitacao = new ArrayList<QtdSolicitacao>();
-							
-							 for(int z = 0; z < 3; z++) {
-								 	count200++;
-								 	Date date = new Date();
-									String dataInfo = dateFormat.format(date);
-									solicitacao.add(new QtdSolicitacao(dataInfo, count200, count400, count500));
-									Thread.sleep(2000);
-									System.out.println("Contando:" + z);
-							 }
-							 StatefulBeanToCsv<QtdSolicitacao> beanToCsv = new StatefulBeanToCsvBuilder(writer).build();
-							 beanToCsv.write(solicitacao);
-							
-							writer.flush();
-							writer.close();
+	    		 for(int z = 0; z < 3; z++) {
+	    			 
+	    		
+		    		switch (response.getStatus()) {
+					
+		    		case 200:						
+						 	count200++;
+						 	Date date = new Date();
+							String dataInfo = dateFormat.format(date);
+							solicitacao.add(new QtdSolicitacao(dataInfo, count200, count400, count500));
+							break;
+		    		case 400:
+						 	count400++;
+						 	Date date400 = new Date();
+							String data400 = dateFormat.format(date400);
+							solicitacao.add(new QtdSolicitacao(data400, count200, count400, count500));
+							break;
+					case 500:
+						 	count500++;
+						 	Date date500 = new Date();
+							String data500 = dateFormat.format(date500);
+							solicitacao.add(new QtdSolicitacao(data500, count200, count400, count500));
 						
-					} catch (IOException e) { 
-			            // TODO Auto-generated catch block 
-			            e.printStackTrace(); 
-			        } 
-					break;
-				case 400:
-					
-				break;
+						
+						if(count500 > count200) {
+							WebResource webUrl_reiniciar = client.resource("https://desafioperformance.b2w.io/reinicia");
+							ClientResponse responseReinicia = webUrl_reiniciar.accept("application/json")
+						            .get(ClientResponse.class);  
+							
+							if(responseReinicia.getStatus() == 200) {
+								
+								logger.info("Serviço reiniciado com sucesso !!!");	
+							}
+						}
+						
+						break;
+					}
+		    		Thread.sleep(2000);
+	    	}
+	    		StatefulBeanToCsv<QtdSolicitacao> beanToCsv = new StatefulBeanToCsvBuilder(writer).build();
+				beanToCsv.write(solicitacao);
 				
-				
-				case 500:
-					
-				default:
-					break;
-				}
-	   
-	    		
+				writer.flush();
+				writer.close();	
+
 	    	}catch (Exception e) {
-				// TODO: handle exception
 	    		e.printStackTrace();
 			}
 			
